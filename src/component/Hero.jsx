3f0398d4142da719080047f75bfd4bd0402f843e
badr ghanbi <bghanbi50@gmail.com>
2022-12-04 15:29:31 +0100

    more work on the hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { robot } from '../assets';
+import { robot,discount} from '../assets';
 function Hero(props) {
     return (
-        <section className='hero md:grid  grid-cols-2 gap-7 p-4 justify-center '>
-            <div className='hero__img-box my-6'>
-                <img className='hero__img w-full md:w-9/12 col-start-2 col-end-4' src={robot} alt="robott image" />
-            </div>
-            <div className='hero__content  place-self-center '>
-                <div className='hero__discountBar'>
-                    <p>20% Discount for 1 month account</p>
+        <section className='hero lg:px-14 mt-16 md:grid   grid-cols-2 gap-7 p-4 justify-center '>
+            
+            <div className='hero__content md:w-full  row-start-1  place-self-center'>
+                <div className='hero__discountBar flex  justify-between items-center px-4 py-1 max-w-fit'>
+                    <img src={discount} alt="discount icon" />
+                    <p className='ml-5'>20% Discount for 1 month account</p>
                 </div>
                 <h1 className='hero__title my-5 capitalize	 leading-normal   text-4xl md:text-7xl	'>
                     the next <strong>genereation</strong> payment methode
                 </h1>
-                <p className='hero__desc'>
+                <p className='hero__desc '>
                     Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.
                     We examine annual percentage rates, annual fees.
                 </p>
+            </div>
+            <div className='hero__img-box my-6 col-start-2 col-end-3'>
+                <img className='hero__img 	 w-full md: col-start-2 col-end-4' src={robot} alt="robott image" />
             </div>
 
         </section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-     
+      screens: {
+        'md': '960px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
