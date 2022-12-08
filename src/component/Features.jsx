991727e576956990d9da48d7c4b0992650e4d04b
badr ghanbi <bghanbi50@gmail.com>
2022-12-08 15:13:10 +0100

    change the column position for the last images

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -17,10 +17,10 @@ function Features(props) {
             title="Easily control your billing & invoicing."
             text="Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat."
             />
-            <FeaturesImage   image={card}/>
             <FeaturesGetStarted 
             title="Easily control your billing & invoicing."
              text="Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat."/>
+              <FeaturesImage   image={card}/>
         </div>
     );
 }
