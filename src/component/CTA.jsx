cfbbe5066bed763aa167163273a0bddefc4f1a3a
badr ghanbi <bghanbi50@gmail.com>
2022-12-09 12:09:57 +0100

    fix some issues

--- src/component/CTA.jsx
+++ src/component/CTA.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <section className='CTA flex  justify-between items-center gray-bg   max-w-6xl md:mx-auto grid-rows-2 grid-cols-3'>
+    <section className='CTA m-10 flex  justify-between items-center gray-bg   max-w-6xl md:mx-auto grid-rows-2 grid-cols-3 my-40'>
         <div className='cta-content col-span-2'>
             <h2 className='text-3xl leading-relaxed font-bold  md:w-3/4  '>Let’s try our service now!</h2>
             <p className='md:w-3/4'>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
